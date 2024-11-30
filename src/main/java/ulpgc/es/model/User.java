59b2d09e1a45bc25a7af144373dbcfe8e39499ff
Saul <exmosaul@gmail.com>
2024-11-30 17:50:26 +0000

    Creación del programa y de la clase del modelo User

---
+++ src/main/java/ulpgc/es/model/User.java
@@ -0,0 +1,46 @@
+package ulpgc.es.model;
+
+public class User {
+
+    public enum Gender{
+        Male,
+        Female
+    }
+
+    private final String name;
+    private final String surname;
+    private final String email;
+    private final Gender gender;
+    private byte[] photo;
+
+    public User(String name, String surname, String email, Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+}
