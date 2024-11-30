254bb99c0f9c3bfdf6659add583eaed5085de945
Saul <exmosaul@gmail.com>
2024-11-30 18:28:13 +0000

    Creación de la interfaz y de la implementación de las clases UserAdapter y RandomUserMeAdapter

--- src/main/java/ulpgc/es/control/RandomUserProvider.java
+++ src/main/java/ulpgc/es/control/RandomUserProvider.java
@@ -1,6 +1,8 @@
 package ulpgc.es.control;
 
+import com.google.gson.Gson;
 import org.jsoup.Jsoup;
+import ulpgc.es.control.pojo.RandomUserMeResponse;
 import ulpgc.es.model.User;
 
 import java.io.IOException;
@@ -11,5 +13,6 @@ public class RandomUserProvider implements UserProvider {
     @Override
     public User provideNew() throws IOException {
         String text = Jsoup.connect(RANDOM_USER_URL).ignoreContentType(true).get().text();
+        RandomUserMeResponse response = new Gson().fromJson(text, RandomUserMeResponse.class);
     }
 }
